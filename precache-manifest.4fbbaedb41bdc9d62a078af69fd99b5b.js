a848e29c9193ec03a4097dd19406db630122c536
Subho891 <90503666+Subho891@users.noreply.github.com>
2022-06-12 13:21:32 +0530

    Updates

---
+++ precache-manifest.4fbbaedb41bdc9d62a078af69fd99b5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2049d88632ce778597ab",
+    "url": "/textify_react/static/css/main.5ef7be70.chunk.css"
+  },
+  {
+    "revision": "2049d88632ce778597ab",
+    "url": "/textify_react/static/js/main.2049d886.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/textify_react/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "f8d9088c16ca9362a4e6",
+    "url": "/textify_react/static/js/2.f8d9088c.chunk.js"
+  },
+  {
+    "revision": "9b1e769fbe4ad43eaae1",
+    "url": "/textify_react/static/js/runtime~main.9b1e769f.js"
+  },
+  {
+    "revision": "d595dfe51aaaf6fa52b8dd615c17ce09",
+    "url": "/textify_react/static/media/logo.d595dfe5.png"
+  },
+  {
+    "revision": "4a4568d3a09873f271f05d6834e8a05c",
+    "url": "/textify_react/index.html"
+  }
+];
